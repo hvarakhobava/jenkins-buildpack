b2db1a383c2475ca8470aa1cab7ee56abd1d9c3b
Hanna <h.varakhobava@gmail.com>
2016-11-03 16:51:57 +0300

    fixed paths

--- bin/copy_id_rsa.sh
+++ bin/copy_id_rsa.sh
@@ -1,13 +1,16 @@
 # copying id_rsa to .ssh
 
-ssh_dir=$HOME/.ssh
+ssh_dir=~/.ssh
 echo "SSH direcotry - $ssh_dir"
-id_rsa_file=$HOME/app/WEB-INF/classes/keys/id_rsa
+id_rsa_file=~/app/WEB-INF/classes/keys/id_rsa
 echo "Id RSA - $id_rsa_file"
 
 echo "Copying ssh keys."
 
 mkdir -p $ssh_dir
+if [ $? == 0 ]; then
+  echo "SSH directory created"
+fi
 if [ -f $id_rsa_file ];
 then
   echo "Copying $id_rsa_file to $ssh_dir"
