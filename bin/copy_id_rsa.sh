b58b834432c39329c0e1d99f521f79af4d1e609f
Hanna <h.varakhobava@gmail.com>
2016-11-03 16:26:36 +0300

    fixed names

--- bin/copy_id_rsa.sh
+++ bin/copy_id_rsa.sh
@@ -9,7 +9,7 @@ echo "Copying ssh keys."
 mkdir -p $ssh_dir
 if [ -f $id_rsa_file ];
 then
-  echo "Copying $id_rsa_file to $ssh_dir_name"
-  cp $id_rsa_file $ssh_dir_name
+  echo "Copying $id_rsa_file to $ssh_dir"
+  cp $id_rsa_file $ssh_dir
   echo $?
 fi
